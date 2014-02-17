fec920d70e73e15e9db9ab984efd44002777894d
Adrian Duyzer <adrianduyzer@gmail.com>
2014-02-17 10:44:44 -0500

    Update comments_controller.rb
    To protect against spambots

--- lib/forge/app/controllers/comments_controller.rb
+++ lib/forge/app/controllers/comments_controller.rb
@@ -5,19 +5,22 @@ class CommentsController < ApplicationController
   def create
      params[:comment].merge!({:user_ip => request.remote_ip, :user_agent => request.env['HTTP_USER_AGENT'], :referrer => request.env['HTTP_REFERER']})
      @comment = Comment.new(params[:comment])
-     if @comment.save
-       flash[:notice] = "Your comment was submitted successfully."
-       flash[:notice] += "  It is awaiting moderation and will be posted soon." if !@comment.approved
-       flash[:notice] += " <a href='#comment-#{@comment.id}'>Jump to it now.</a>" if @comment.approved
-       session[:comment] = ""
-       session[:comment_errors] = ""
-       redirect_to @comment.referrer and return false
-     else
+     begin
+       if @comment.save
+         flash[:notice] = "Your comment was submitted successfully."
+         flash[:notice] += "  It is awaiting moderation and will be posted soon." if !@comment.approved
+         flash[:notice] += " <a href='#comment-#{@comment.id}'>Jump to it now.</a>" if @comment.approved
+         session[:comment] = ""
+         session[:comment_errors] = ""
+       else
+         flash[:warning] = "There was a problem adding your comment. Check the comment form for more details."
+         session[:comment] = params[:comment]
+         session[:comment_errors] = @comment.error_message
+       end
+     rescue NameError
        flash[:warning] = "There was a problem adding your comment. Check the comment form for more details."
-       session[:comment] = params[:comment]
-       session[:comment_errors] = @comment.error_message
-       redirect_to @comment.referrer
      end
+     redirect_to @comment.referrer
    end
 
    def destroy
